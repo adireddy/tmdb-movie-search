0dbff73d80a768828eec4ff03b04f7788a7ed53a
Adi <adiinteractive@gmail.com>
2016-07-21 19:38:18 +0100

    base project and package setup

---
+++ js/tmdb-search.js
@@ -0,0 +1,10 @@
+(function (console) { "use strict";
+var org_themoviedb_Main = function() {
+};
+org_themoviedb_Main.main = function() {
+	new org_themoviedb_Main();
+};
+org_themoviedb_Main.main();
+})(typeof console != "undefined" ? console : {log:function(){}});
+
+//# sourceMappingURL=tmdb-search.js.map
---
+++ src/org/themoviedb/Main.hx
@@ -0,0 +1,12 @@
+package org.themoviedb;
+
+class Main {
+
+	public function new() {
+
+	}
+
+	static function main() {
+		new Main();
+	}
+}
