a9a65585aed4343455b5ce8c4bd5ed59f8b9ed23
Adi <adiinteractive@gmail.com>
2016-07-21 20:29:06 +0100

    added TMDb call to get selected movie details

--- src/org/tmdb/components/moviedetails/MovieDetailsController.hx
+++ src/org/tmdb/components/moviedetails/MovieDetailsController.hx
@@ -1,16 +1,30 @@
 package org.tmdb.components.moviedetails;
 
+import org.tmdb.model.Model;
+import externs.tmdb.TMDb;
+
 class MovieDetailsController implements IComponentController {
 
-	public function new() {
+	@inject public var model:Model;
+	@inject public var view:MovieDetailsView;
 
-	}
+	public function new() {}
 
 	public function init() {
 
 	}
 
-	public function reset() {
+	function _onGetMovieDetails(id:Int) {
+		TMDb.movies.getById({ id:id }, _onMovieDetailsSuccess, _onError);
+	}
+
+	function _onMovieDetailsSuccess(response:Dynamic) {
 
 	}
+
+	function _onError(response:Dynamic) {
+		
+	}
+
+	public function reset() {}
 }
