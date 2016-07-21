4f8e961b7a2780def05d2fa9c7506cf13671bf8e
Adi <adiinteractive@gmail.com>
2016-07-21 22:01:00 +0100

    added bindable searchString to hide movie details when searching

--- src/org/tmdb/model/Model.hx
+++ src/org/tmdb/model/Model.hx
@@ -10,10 +10,16 @@ class Model implements IBindable {
 	public static inline var API_KEY:String = "e50938fa4e13e8b1b31c287c1b820574";
 
 	@:bindable public var selectedMovie:Int;
+	@:bindable public var searchString:String;
 
 	public function new() {}
 
-	public function init() {}
+	public function init() {
+		searchString = null;
+		selectedMovie = -1;
+	}
 
-	public function reset() {}
+	public function reset() {
+		init();
+	}
 }
