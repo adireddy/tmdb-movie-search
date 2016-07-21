3b90f807fa9160b258e42966b83dc2fa5309b82e
Adi <adiinteractive@gmail.com>
2016-07-21 20:28:46 +0100

    added TMDb call to get search results

--- src/org/tmdb/components/search/SearchController.hx
+++ src/org/tmdb/components/search/SearchController.hx
@@ -1,16 +1,34 @@
 package org.tmdb.components.search;
 
+import externs.tmdb.TMDb;
+import org.tmdb.model.Model;
+
+using StringTools;
+
 class SearchController implements IComponentController {
 
-	public function new() {
+	@inject public var model:Model;
+	@inject public var view:SearchView;
 
-	}
+	public function new() {}
 
 	public function init() {
 
 	}
 
-	public function reset() {
+	function _onSearch(val:String) {
+		if (val.trim() != "") {
+			TMDb.search.getMovie({query:val}, _onSearchSuccess, _onError);
+		}
+	}
+
+	function _onSearchSuccess(response:Dynamic) {
 
 	}
+
+	function _onError(response:Dynamic) {
+
+	}
+
+	public function reset() {}
 }
